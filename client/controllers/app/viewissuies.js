af9ee869e85fa6f657d1dd51e7743f97dc0e2ed1
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-23 08:08:59 +0000

    [Studio] Add screen "Viewissuies"

---
+++ client/controllers/app/viewissuies.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_viewissuies', app_viewissuies);
+function app_viewissuies($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -42,5 +42,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.viewissuies', {
+        views: {
+            app: {
+                controller: 'app_viewissuies',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.viewissuies');
+                }
+            }
+        }
     });
 });
