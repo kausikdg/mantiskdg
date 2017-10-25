86507553cdb288165d75d2a94ac96d13de7c5b6f
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 02:45:44 +0000

    [Studio] Modified screen "viewissues"

--- client/controllers/app/viewissues.js
+++ client/controllers/app/viewissues.js
@@ -1,6 +1,7 @@
 angular.module('app').controller('app_viewissues', app_viewissues);
 function app_viewissues($scope, app) {
     'use strict';
+    
     app.init($scope, function() {
         var i = $scope;
     });
