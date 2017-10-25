55285ed398166df819ba2ddc91e3cc4ab7b61afc
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 02:56:20 +0000

    [Studio] Modified screen "Login"

--- client/controllers/app/login.js
+++ client/controllers/app/login.js
@@ -1,11 +1,12 @@
 angular.module('app').controller('app_login', app_login);
 function app_login($scope, app, $q) {
     'use strict';
-    app.init($scope);
+    app.init($scope, function() {
+        $scope.data.username = 'Administrator';
+        $scope.data.password = 'powwow';        
+    });
     
     $scope.flex = function() {
-        $scope.data.username = 'Administrator';
-        $scope.data.password = 'powwow';
         app.action('login', 'submit', this);
     }
     
